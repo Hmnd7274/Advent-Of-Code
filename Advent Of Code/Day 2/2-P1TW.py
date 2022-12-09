e86b747ca4c969fecc77c9ded9993ea843f5c698
Dragoncode333 <103138581+Dragoncode333@users.noreply.github.com>
2022-12-09 11:51:40 +0100

    Update 2-P1TW.py

--- Advent Of Code/Day 2/2-P1TW.py
+++ Advent Of Code/Day 2/2-P1TW.py
@@ -8,13 +8,23 @@ answer = 0
 while i < 2499:
 
   print(res)
-
+  
+  if res == str("A Y"):
+    answer = answer + int(6) + int(2)
+  if res == str("B Z"):
+    answer = answer + int(6) + int(3)
+  if res == str("C X"):
+    answer = answer + int(6) + int(3)
+    
+  if res == str("A X"):
+    answer = answer + int(4)
+    print(answer)
+    
   if res == str("A X"):
     answer = answer + int(4)
     print(answer)
 
-  # if res == str("A Y"):
-  #   answer = answer + int(8)
+
   
   del myList[0]
 
